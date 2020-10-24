f31c243b0f2fa9bedebd149df823d1c41498010c
TechStuffBoy <nandhu03.m@gmail.com>
2020-10-24 12:00:25 +0530

    Updates

---
+++ precache-manifest.1b891776603e53d39acb53c36b1d94c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7566b1896f51a87e57c0de6464937b13",
+    "url": "/tic_tac_react/index.html"
+  },
+  {
+    "revision": "dcff0d338cb438afb586",
+    "url": "/tic_tac_react/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "42cc29e2d1cd191c817a",
+    "url": "/tic_tac_react/static/js/2.1bc5cf13.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/tic_tac_react/static/js/2.1bc5cf13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcff0d338cb438afb586",
+    "url": "/tic_tac_react/static/js/main.536c1c45.chunk.js"
+  },
+  {
+    "revision": "5a37fd63cd601db40b90",
+    "url": "/tic_tac_react/static/js/runtime-main.1016abab.js"
+  }
+]);
